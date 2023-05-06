91dd44c397a35a715becde9759e5ff26434a0699
refaat1297 <a.refaat1297@gmail.com>
2023-05-06 17:29:10 +0300

    add github extends

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  extends: '../shared-repo',
+  // extends: '../shared-repo',
+  extends: 'github:refaat1297/shared-repo',
 })
