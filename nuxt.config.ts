ac225f949d493fca1ccf8b3d7f59e7a0a3f572c4
refaat1297 <a.refaat1297@gmail.com>
2023-05-07 09:40:01 +0300

    add env var

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   // extends: '../shared-repo',
-  extends: 'github:refaat1297/shared-repo',
+  // extends: 'github:refaat1297/shared-repo',
+  // extends: '../new-shared-repo',
+  extends: 'bitbucket:refaat1297-workspace/workspace-shared',
+
+  runtimeConfig: {
+    public: {
+      GIGET_AUTH: process.env.GIGET_AUTH,
+    }
+  }
 })
